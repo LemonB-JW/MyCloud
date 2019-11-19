f22ff7a55f0925bd9312c5678b69487f24743e57
Jiaying Guo <gjying@seas.upenn.edu>
2019-11-18 23:15:20 -0500

    modified makefile

--- lib/BigTable.h
+++ lib/BigTable.h
@@ -4,34 +4,30 @@
 #include <unistd.h>
 #include <string.h>
 #include <unordered_map> 
-
+#include "../lib/TableCell.h"
 
 using namespace std;
 
 class BigTable
 {
 public:
-	unordered_map<string, unordered_map<string, Cell*>> table; // row key: user name, col key: fileID (every folder has a unique ID: workerIndex + sequenceNum + timestamp
+	unordered_map<string, unordered_map<string, TableCell*>> table; // row key: user name, col key: fileID (every folder has a unique ID: workerIndex + sequenceNum + timestamp
 	// unordered_map<string, FileData*> files_in_path;
 	BigTable(){
-
 	};
 	~BigTable(){
-		for(unordered_map<string, unordered_map<string, FileData*>*>::iterator itr = table.begin();itr !=  table.end();itr++){
-			delete itr->second;
-		}
 	}
 
 	// PUT(r,c,v): Stores a value v in column c of row r
-	bool put(string row, string col, string data);
+	bool put(string row, string col, string* data);
 
 	// GET(r,c): Returns the value stored in column c of row r
 	string get(string row, string col);
 
 	// CPUT(r,c,v1,v2): Stores value v2 in column c of row r, but only if the current value is v1
-	bool cput(string row, string col, string old_data, string new_data);
+	bool cput(string row, string col, string* old_data, string* new_data);
 
 	// DELETE(r,c): Deletes the value in column c of row r
-	bool delete(string row, string col);
+	bool table_delete(string row, string col);
 
 };
