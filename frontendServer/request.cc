9f12c0c0247acac62e923bbf5efae80fd433019d
Xuan Wang <xuanw@seas.upenn.edu>
2019-11-22 00:11:11 -0500

    add cookie support in http request

--- frontendServer/request.cc
+++ frontendServer/request.cc
@@ -19,21 +19,6 @@
 #include "utils/constants.h"
 //#include "utils.h"
 
-//
-//GET /consolke HTTP/1.1
-//Host: localhost:8080
-//Connection: keep-alive
-//        Cache-Control: max-age=0
-//Upgrade-Insecure-Requests: 1
-//User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36
-//Sec-Fetch-User: ?1
-//Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3
-//Sec-Fetch-Site: none
-//Sec-Fetch-Mode: navigate
-//Accept-Encoding: gzip, deflate, br
-//Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7
-//Cookie: Idea-465b027b=dcd70f3e-fe5a-49dd-96b8-c4453861fbee
-
 Request::Request(int fd) {
     char *buf = (char *)malloc(sizeof(char) * MAX_REQUEST_LENGTH);
 
@@ -41,6 +26,11 @@ Request::Request(int fd) {
     std::string request = std::string(buf);
     parse_req_string(*this, request);
 
+    this->cookie = headers["Cookie"];
+    //    for (auto key_val : headers) {
+//        std::cout << "key: //" << key_val.first << "//" << " val: //" << key_val.second << "//" << std::endl;
+//
+//    }
     if (buf != NULL) {
         free(buf);
     }
@@ -48,11 +38,7 @@ Request::Request(int fd) {
 
 /* parse incoming request string into structured request object */
 void Request::parse_req_string(Request &req, std::string req_str) {
-
-    size_t pos = req_str.find(CRLF);
-//    std::string start_line = req_str.substr(0, pos);
-
-    size_t prior_pos;
+    size_t pos, prior_pos;
 
     // 1. parse start line: method, url, http version
     pos = req_str.find(" ");
@@ -67,19 +53,22 @@ void Request::parse_req_string(Request &req, std::string req_str) {
     req.version = req_str.substr(prior_pos + 1, pos - prior_pos - 1);
     prior_pos = pos;
 
+    std::string headers = req_str.substr(prior_pos + 1, req_str.length() - prior_pos);
+
     // 2. parse headers
-    while (pos <= req_str.length()) {
-        pos = req_str.find(":", prior_pos + 1);
-        std::string key = req_str.substr(prior_pos + 1, pos - prior_pos);
+    prior_pos = 0;  // reset prior_pos
+    while (pos >= headers.length() || pos != std::string::npos) {
+        pos = headers.find(":", prior_pos + 1);
+        std::string key = headers.substr(prior_pos + 1, pos - prior_pos - 1);
         prior_pos = pos;
 
-        pos = req_str.find("\n", prior_pos + 1);
-        std::string val = req_str.substr(prior_pos + 1, pos - prior_pos);
+        if (pos >= headers.length()) {
+            break;
+        }
+
+        pos = headers.find("\n", prior_pos + 1);
+        std::string val = headers.substr(prior_pos + 2, pos - prior_pos - 3);
         prior_pos = pos;
-
-        std::cout << "val: " << val <<std::endl;
-
-        std::cout << "pos: " << pos << "length: " << req_str.length() << std::endl;
 
         req.headers[key] = val;
     }
