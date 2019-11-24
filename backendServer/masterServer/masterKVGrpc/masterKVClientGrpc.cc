1c833f3eec63167f423ecf6b0781e4b0ff198770
Peng Li <pl279@seas.upenn.edu>
2019-11-23 21:52:28 -0500

    grpc master and backend

---
+++ backendServer/masterServer/masterKVGrpc/masterKVClientGrpc.cc
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <functional>
+
+#include <grpcpp/grpcpp.h>
+#include "masterBackend.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using masterBackend::MasterBackend;
+using masterBackend::GetPrimRequest;
+using masterBackend::GetPrimReply;
+using masterBackend::ListSubRequest;
+using masterBackend::ListSubReply;
+
+class MBcommuClient {
+	public:
+		MBcommuClient(std::shared_ptr<Channel> channel):stub_(MasterBackend::NewStub(channel)){}
+	
+	int get_prim(int myindex){
+		GetPrimRequest request;
+		
+		request.set_my_index(myindex);
+		ClientContext context;
+		GetPrimReply reply;
+		Status status = stub_->get_prim(&context, request, &reply);
+		
+		if(status.ok()){
+			return reply.prim_index();
+		}else{
+			std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+			return -1;
+		}
+	}
+	
+	std::vector<int> list_sub(int myindex){
+		ListSubRequest request;
+		
+		request.set_my_index(myindex);
+		ClientContext context;
+		ListSubReply reply;
+		Status status = stub_->list_sub(&context, request, &reply);
+		
+		if(status.ok()){
+			std::vector<int> response;
+			for (int i = 0; i< reply.sub_index_size(); i++){
+				response.push_back(reply.sub_index(i));
+			}
+			return response;
+		}else{
+			std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+			std::vector<int> err;//return empty vector when error???
+			return err;
+		}
+	}
+	
+	private:
+		std::unique_ptr<MasterBackend::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created. This channel models a connection to an endpoint (in this case,
+  // 0.0.0.0:5000). We indicate that the channel isn't authenticated
+  // (use of InsecureChannelCredentials()).
+  std::string address("127.0.0.1:10001");//here shoule be the master address and port
+  MBcommuClient client(
+  	grpc::CreateChannel(
+      address,
+      grpc::InsecureChannelCredentials()
+    )
+  );
+  int myIndex = 1;
+  //fprintf(stderr, "ready to send alice to server\n");
+  int primeIndex = client.get_prim(myIndex);
+  std::vector<int> sublist = client.list_sub(primeIndex);
+  
+  std::cout << "Client received: " << primeIndex << "  sub list is "<<std::endl;
+	for (int i = 0; i <  sublist.size(); i++){
+		std::cout << sublist[i] << " " <<std::endl;
+	}
+  return 0;
+}
---
+++ backendServer/masterServer/masterKVGrpc/masterKVGrpc.cc
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <unistd.h>//getopt
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+#include <stdlib.h>
+#include <netinet/in.h>
+
+#include <string.h>//strerror
+#include <pthread.h>
+#include <functional>
+
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#include "../../../lib/masterInfo.h"
+
+#include "masterBackend.grpc.pb.h"
+
+
+#ifndef panic
+#define panic(a...) do { fprintf(stderr, a); fprintf(stderr, "\n"); exit(1); } while (0) 
+#endif
+
+#define MAX_CONNECTIONS 100
+#define MAX_COMMAND_LENGTH 1000
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using masterBackend::MasterBackend;
+using masterBackend::GetPrimRequest;
+using masterBackend::GetPrimReply;
+using masterBackend::ListSubRequest;
+using masterBackend::ListSubReply;
+
+
+bool shutDown = false; //true if ctrl-c received
+bool debugMode = false; //print debug msg if true
+int socketfd;
+masterInfo myInfo;
+
+class MFcommu final : public MasterBackend::Service{
+	Status get_prim(
+		ServerContext* context,
+		const GetPrimRequest* request,
+		GetPrimReply* reply
+	)override{
+		//std::string username = getServerList(request->user_name());
+		int primInd = myInfo.getPrime(request->my_index());
+		reply->set_prim_index(primInd);
+		return Status::OK;
+	}
+	
+	Status list_sub(
+		ServerContext* context,
+		const ListSubRequest* request,
+		ListSubReply* reply
+	)override{
+		std::vector<int> sublist = myInfo.getSub(request->my_index());
+		for (int i = 0; i < sublist.size(); i++){
+			reply->add_sub_index(sublist[i]);
+			//reply->set_sub_index(sublist[i]);
+		}
+		return Status::OK;
+	}
+};
+
+void Run();
+
+int main(int argc, char** argv){
+	/* Parse arguments */
+  if (argc < 2)
+    panic("*** Final Project T08, Author: Peng Li (pl279)\n");
+  /* Your code here */
+  /************ Signal handler *************/
+  /*struct sigaction sigIntHandler;
+  sigIntHandler.sa_handler = sigintHandler;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sigIntHandler, NULL);
+	*/
+  /************ Parse arguments *************/
+	int opt;
+	while((opt = getopt(argc, argv, "v")) != -1){
+		switch(opt){
+			case 'v':
+				debugMode = true;
+				break; 
+			default:
+				panic("Syntax: %s [-v] <serverConfigFile>\n", argv[0]);
+		}	
+	}
+	
+  if (optind >= argc){
+		panic("Error: config file is missing!\n");
+	}
+	/* Read the server list */
+	myInfo.readConfig(argv[optind]);
+
+	Run();
+	return 0;
+}
+
+
+void Run(){
+	//std::string address("0.0.0.0:5000");
+	std::string address("127.0.0.1:10001");
+	fprintf(stderr, "into run\n");
+	MFcommu service;
+	
+	//bind server to port and service
+	ServerBuilder builder;
+	
+	builder.AddListeningPort(address, grpc::InsecureServerCredentials());
+	builder.RegisterService(&service);
+	
+	std::unique_ptr<Server> server(builder.BuildAndStart());
+	fprintf(stderr, "Server listening on port: %s\n", address.c_str());
+	
+	server->Wait();
+}
+
