9827210379ae22c43d2ce3cc4e8d974521e5866b
Peng Li <pl279@seas.upenn.edu>
2019-11-25 00:30:47 -0500

    fix masterInfo.cc bug

--- backendServer/masterServer/masterServerGrpc/masterInfo.cc
+++ backendServer/masterServer/masterServerGrpc/masterInfo.cc
@@ -73,7 +73,6 @@ void masterInfo::setPrimary(){
 			replicaInfo[primaryIdx].push_back(primaryIdx+i);
 			myprimary[primaryIdx+i] = primaryIdx;
 		}
-		groupID++;
 		groupNum = groupID + 1;
 		remainS = 0;		
 	}
